896a57b71996bd49916e830623d5499f96139fbc
adnananik <adnananik301@gmail.com>
2026-06-12 16:19:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// This is a test Javascript file to demonstrate a simple calculator function.
+
+
+let firstname = "Adnan";
+let lastname = "Anik";
+let age = 25;
+let nextage = age + 1  ;
+let phone = "123-456-7890";
+let email = "adnananik301@gmail.com";
+console.log(`My name is ${firstname} ${lastname}`);
+
+function add(age, nextage) {
+    return age + nextage;
+}
+console.log(`Next year, I will be ${add(age, nextage)} years old.`);
