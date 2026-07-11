96744b6064e9d988840465f24fbb0f02ced9f566
Adnan Anik <75451616+4dn4n4n1k@users.noreply.github.com>
2026-07-11 23:03:21 +0600

    Delete practice/javascript/Lt-1/extra/1/form validation directory

--- practice/javascript/Lt-1/extra/1/form validation/script.js
+++
@@ -1,9 +0,0 @@
-const fname = document.getElementById("fname");
-const lname = document.getElementById("lname");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const cPassword = document.getElementById("cPassword");
-const phone = document.getElementById("phone");
-const form = document.getElementById("signupForm");
-const result = document.getElementById("result");
-
